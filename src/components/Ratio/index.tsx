55e3ebadffea22d1cca9d0614d6482991d560514
manhir <53478716+manhir@users.noreply.github.com>
2020-05-08 18:02:36 +0300

    plushki content

--- src/components/Ratio/index.tsx
+++ src/components/Ratio/index.tsx
@@ -5,17 +5,21 @@ interface RatioProps {
     left: string | number
     right: string | number
     style?: CSSProperties
+    spacer?: string | number
 
     leftContent?: React.ReactNode
     rightContent?: React.ReactNode
 }
 
-export const Ratio: React.FC<RatioProps> = props => {
+export const Ratio: React.FC<RatioProps> = ({ spacer = undefined, ...props }) => {
     return (
         <div className={s.container} style={props.style}>
             <div style={{ flex: props.left }}>
                 {props.leftContent}
             </div>
+
+            {!spacer ? null : <div style={{ flex: spacer }} />}
+
             <div style={{ flex: props.right }}>
                 {props.rightContent}
             </div>
