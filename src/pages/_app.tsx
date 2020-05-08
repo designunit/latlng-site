32493230fbbaf75af3c50047608ea545dc863241
manhir <53478716+manhir@users.noreply.github.com>
2020-05-08 15:37:47 +0300

    styles reset & default styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
+import '../styles/style.css'
+
 import Head from 'next/head'
 import { AppType } from 'next/dist/next-server/lib/utils'
+
+
 const App: AppType = props => {
     const { Component, pageProps } = props
 
