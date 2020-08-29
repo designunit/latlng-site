83e7dc567a88bc35f1bb07b155087907024bde60
Roman Timashev <roman@tmshv.ru>
2020-08-29 11:43:14 +0300

    extract useSpeedCurve hook

--- src/components/Zalipuha/index.tsx
+++ src/components/Zalipuha/index.tsx
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic'
 import { useEffect, useState, forwardRef, MutableRefObject } from 'react'
 import { GeoMarker } from './Geosphere'
 import { useRafLoop } from 'react-use'
+import { useSpeedCurve } from './lib'
 
 const Geosphere = dynamic(import('./Geosphere').then(m => m.Geosphere), {
     ssr: false,
@@ -13,10 +14,12 @@ interface ZalipuhaProps {
 type Ref = HTMLDivElement
 
 export const Zalipuha = forwardRef<Ref, ZalipuhaProps>((props, ref: MutableRefObject<Ref>) => {
-    const [rotation, setRotation] = useState(0)
-    const [velocity, setVelocity] = useState(0.25)
-    const [mouse, setMouse] = useState(false)
-    const mouseSpeed = .1
+    const [rotation, , , setAcceleration] = useSpeedCurve({
+        initialAcceleration: 0.25,
+        stableAcceleration: 0.05,
+        dump: 0.99,
+        startingDump: 1.025,
+    })
     const points: GeoMarker[] = [
         { location: [-12.471514065069812, 29.96032823460071], imageSrc: '/static/cats/1.jpg' },
         { location: [-66.14344941751239, -33.78218055874478], imageSrc: '/static/cats/2.jpg' },
@@ -40,59 +43,22 @@ export const Zalipuha = forwardRef<Ref, ZalipuhaProps>((props, ref: MutableRefOb
         { location: [154.88855888496272, -45.3358210847797], imageSrc: '/static/cats/4.jpg' },
     ]
 
-    const [stop, start, isActive] = useRafLoop(time => {
-        setRotation(rotation + velocity)
-        setVelocity(v => {
-            // Stable velocity in no interaction state
-            const D = 0.05
-
-            // Default velocity dump
-            let m = 0.99
-
-            // Reverse negative velocity if it almost 0
-            if (v < 0 && v > -0.001) {
-                m = -1
-            }
-
-            // Speed up velocity if it less than D
-            if (v > 0 && v < D) {
-                m = 1.025
-            }
-
-            return v * m
-        })
-    })
-
     useEffect(() => {
-        console.log('my target is ', ref.current)
         const target = ref.current
 
-        const mouseDown = () => {
-            setMouse(true)
-        }
-
-        const mouseUp = () => {
-            setMouse(false)
-        }
-
         const mouseMove = function (event: MouseEvent) {
+            const mouseSpeed = 0.1
             const eventDelta = event.movementX * mouseSpeed * devicePixelRatio
             if (event.buttons !== 1) {
                 return
             }
 
-            setVelocity(eventDelta)
+            setAcceleration(eventDelta)
         }
 
-        target.addEventListener('mousedown', mouseDown)
-        target.addEventListener('mouseup', mouseUp)
-        target.addEventListener('mouseleave', mouseUp)
         target.addEventListener('mousemove', mouseMove)
 
         return () => {
-            target.removeEventListener('mousedown', mouseDown)
-            target.removeEventListener('mouseup', mouseUp)
-            target.removeEventListener('mouseleave', mouseUp)
             target.removeEventListener('mousemove', mouseMove)
         }
     }, [ref])
--- src/components/Zalipuha/lib.ts
+++ src/components/Zalipuha/lib.ts
@@ -1,9 +1,56 @@
 import { geoOrthographic, geoGraticule10, GeoProjection } from 'd3-geo'
-import { useRef } from 'react'
+import { useRef, useState, Dispatch, SetStateAction } from 'react'
+import { useRafLoop } from 'react-use'
 
 export function useGeoshere(): [GeoProjection, GeoJSON.MultiLineString] {
     const projection = useRef(geoOrthographic())
     const wireframe = useRef(geoGraticule10())
 
     return [projection.current, wireframe.current]
+}
+
+type UseSpeedCurveOptions = {
+    initialAcceleration: number
+    stableAcceleration: number
+    dump: number
+    startingDump: number
+}
+
+type UseSpeedCurveResult = [
+    number,
+    Dispatch<SetStateAction<number>>,
+    number,
+    Dispatch<SetStateAction<number>>,
+]
+export function useSpeedCurve(options: UseSpeedCurveOptions): UseSpeedCurveResult {
+    const [velocity, setVelocity] = useState(0)
+    const [acceleration, setAcceleration] = useState(options.initialAcceleration)
+
+    const [stop, start, isActive] = useRafLoop(time => {
+        setVelocity(velocity + acceleration)
+        setAcceleration(a => {
+            // Stable velocity in no interaction state
+            const A = options.stableAcceleration
+            // 0.05
+
+            // Default velocity dump
+            let m = options.dump
+            // 0.99
+
+            // Reverse negative velocity if it almost 0
+            if (a < 0 && a > -0.001) {
+                m = -1
+            }
+
+            // Speed up velocity if it less than D
+            if (a > 0 && a < A) {
+                m = options.startingDump
+                // 1.025
+            }
+
+            return a * m
+        })
+    })
+
+    return [velocity, setVelocity, acceleration, setAcceleration]
 }
