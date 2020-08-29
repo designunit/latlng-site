8a30d3643cb3d10902258617d06c896e8791920a
Roman Timashev <roman@tmshv.ru>
2020-08-29 11:50:23 +0300

    move points definition to getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,12 @@ import Head from 'next/head'
 import { Meta, IMeta } from '../components/Meta'
 import { GalleryItem } from '@/app/types'
 import { Zalipuha } from '@/components/Zalipuha'
+import { GeoMarker } from '@/components/Zalipuha/Geosphere'
 
 type Props = {
     meta: IMeta
     examples: GalleryItem[]
+    points: GeoMarker[]
 }
 
 const Index: NextPage<Props> = props => {
@@ -29,6 +31,7 @@ const Index: NextPage<Props> = props => {
             </Head>
             <Zalipuha
                 ref={mouseTargetRef}
+                points={props.points}
             />
             <main style={{
                 position: 'relative',
@@ -125,10 +128,34 @@ export const getStaticProps: GetStaticProps<Props> = async ctx => {
         },
     ]
 
+    const points: GeoMarker[] = [
+        { location: [-12.471514065069812, 29.96032823460071], imageSrc: '/static/cats/1.jpg' },
+        { location: [-66.14344941751239, -33.78218055874478], imageSrc: '/static/cats/2.jpg' },
+        { location: [-163.20801211427406, 75.19405530640374], imageSrc: '/static/cats/3.jpg' },
+        { location: [-60.1468537118777, 31.68887138113911], imageSrc: '/static/cats/4.jpg' },
+        { location: [98.919861719307818, 34.327280220478983], imageSrc: '/static/cats/5.jpg' },
+        { location: [19.505171745122794, 68.34049434153212], imageSrc: '/static/cats/6.jpg' },
+        { location: [112.2117919210032, -28.50740286402985], imageSrc: '/static/cats/7.jpg' },
+        { location: [-180.72891972601244, 18.236456875202094], imageSrc: '/static/cats/8.jpg' },
+        { location: [-144.70567276094468, 42.757649149605925], imageSrc: '/static/cats/1.jpg' },
+        { location: [16.677754391339704, 23.15973160196448], imageSrc: '/static/cats/2.jpg' },
+        { location: [123.39126029072082, 23.943962457354814], imageSrc: '/static/cats/3.jpg' },
+        { location: [34.53401051124965, -35.26060695811529], imageSrc: '/static/cats/4.jpg' },
+        { location: [-54.89336410836961, 7.03808627812122], imageSrc: '/static/cats/5.jpg' },
+        { location: [167.93694930066998, -7.75296599062257], imageSrc: '/static/cats/6.jpg' },
+        { location: [50.24396406944393, 30.558119182431568], imageSrc: '/static/cats/7.jpg' },
+        { location: [-136.37355654473666, -18.713657944900017], imageSrc: '/static/cats/8.jpg' },
+        { location: [88.72475330521985, 10.451665787038394], imageSrc: '/static/cats/1.jpg' },
+        { location: [72.42099035441424, -29.84503313630067], imageSrc: '/static/cats/2.jpg' },
+        { location: [-92.40387452005182, 20.1722585434017], imageSrc: '/static/cats/3.jpg' },
+        { location: [154.88855888496272, -45.3358210847797], imageSrc: '/static/cats/4.jpg' },
+    ]
+
     return {
         props: {
             meta,
             examples,
+            points,
         }
     }
 }
