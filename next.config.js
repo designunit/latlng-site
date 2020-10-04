ed68360bab8e1a1d9971f5f6fa6af458b442e63d
Roman Timashev <roman@tmshv.ru>
2020-10-04 16:59:15 +0300

    add murmansk redirect to next conf

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer()
+module.exports = withBundleAnalyzer({
+    async redirects() {
+        return [
+           {
+               source: '/murmansk',
+               destination: 'https://app.latl.ng/map/FGF6683DZD8R4GE4',
+               permanent: true,
+           }
+        ]
+    }
+})
